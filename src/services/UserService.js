fbdefcc8cb72641de53b72d1af141e5f5b15060d
Balázs Szabó <balazs.szabo@gmail.com>
2018-01-10 08:36:02 +0200

    oaut config

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,18 +1,21 @@
 let querystring = require('querystring')
 import Vue from 'vue'
 
-let clientSecret = document.head.querySelector('meta[name="client-secret"]')
-clientSecret = (clientSecret) ? clientSecret.content : ''
-
 let oauthUrl = document.head.querySelector('meta[name="oauth-url"]')
 oauthUrl = (oauthUrl) ? oauthUrl.content : ''
+
+let clientId = document.head.querySelector('meta[name="oauth-client-id"]')
+clientId = (clientId) ? clientId.content : ''
+
+let clientSecret = document.head.querySelector('meta[name="oauth-client-secret"]')
+clientSecret = (clientSecret) ? clientSecret.content : ''
 
 export const UserService = {
   authUser: function (auth) {
     return new Promise((resolve, reject) => {
       let data = {
         grant_type: 'password',
-        client_id: 2,
+        client_id: clientId,
         client_secret: clientSecret,
         scope: '',
         username: auth.email,
